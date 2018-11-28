8e852709b52c42519f55ba12c1e346c4b35ac010
李志钟 <aichaellee@mich.local>
2018-11-28 15:25:29 +0800

    prepare to reset authorities

--- hrserver/src/main/java/org/sang/common/HrUtils.java
+++ hrserver/src/main/java/org/sang/common/HrUtils.java
@@ -9,6 +9,5 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class HrUtils {
     public static Hr getCurrentHr() {
         return (Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        SecurityContextHolder.getContext().setAuthentication();
     }
 }
