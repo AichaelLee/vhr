aec7386efbccbf2d65b3adfa8ae3af903ed802b2
aichaellee <aichaellee@sina.com>
2018-12-03 13:10:00 +0800

    yes

--- vuehr/src/components/system/SysBasic.vue
+++ vuehr/src/components/system/SysBasic.vue
@@ -1,17 +1,8 @@
 <template>
   <div style="margin-top: 10px">
     <el-tabs v-model="defaultTab" type="card">
-      <el-tab-pane label="部门管理" name="depMana">
-        <dep-mana></dep-mana>
-      </el-tab-pane>
-      <el-tab-pane label="职位管理" name="positionMana">
-        <pos-mana state="position"></pos-mana>
-      </el-tab-pane>
-      <el-tab-pane label="职称管理" name="jobTitleMana">
+      <el-tab-pane label="教师管理" name="jobTitleMana">
         <pos-mana state="jobtitle"></pos-mana>
-      </el-tab-pane>
-      <el-tab-pane label="奖惩规则" name="ecCfg">
-        <ec-mana></ec-mana>
       </el-tab-pane>
       <el-tab-pane label="权限组" name="menuRole">
         <menu-role></menu-role>
@@ -21,23 +12,17 @@
 </template>
 <script>
   import MenuRole from './basic/MenuRole.vue'
-  import DepMana from './basic/DepMana.vue'
-  import ECMana from './basic/ECMana.vue'
   import JobTitleMana from './basic/JobTitleMana.vue'
-  import PosMana from './basic/PosMana.vue'
   export default {
     data() {
       return {
-        defaultTab: 'depMana'
+        defaultTab: 'MenuRole'
       };
     },
     methods: {},
     components: {
       'menu-role': MenuRole,
-      'dep-mana': DepMana,
-      'ec-mana': ECMana,
-      'jobtitle-mana': JobTitleMana,
-      'pos-mana': PosMana
+      'jobtitle-mana': JobTitleMana
     }
   };
 </script>
