4435412c047f3be6c464ef486cab018292ff8088
aichaellee <aichaellee@sina.com>
2018-12-17 13:42:27 +0800

    prepare to add sercutiy module

--- vuehr/src/components/statistics/StMutualSelect.vue
+++ vuehr/src/components/statistics/StMutualSelect.vue
@@ -1,7 +1,47 @@
 <template>
   <div>
     <h1>
-      CNKI-GM 师生双选管理
+      查看所有process 实例流程
+      //processDefinitionId
+      deploymentKey "thesisapproval"
+      state "running"
+      sort "created-desc"
+      page 0
+      size
+      返回的数据为：
+      {{instanceData}}
     </h1>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      instanceData:''
+    }
+  },
+  mounted(){
+    this.getProcessInstance();
+  },
+  methods:{
+    getProcessInstance(){
+      // get all process-instance 
+    this.postRequest('/system/basic/query/process-instances', {
+          deploymentKey: 'thesisapproval',
+          state: 'running',
+          sort: 'created-desc',
+          page:0
+        }).then(resp=> {
+          if (resp && resp.status == 200) {
+            this.instanceData = resp.data;
+          
+           
+          }
+        });
+
+    }
+
+  }
+}
+</script>
+
