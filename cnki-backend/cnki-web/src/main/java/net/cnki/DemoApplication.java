5982e488dde0da35638ee132e82aa6667641fcc9
aichaellee <793708025@qq.com>
2018-12-24 10:08:51 +0800

    remove userless package

--- cnki-backend/cnki-web/src/main/java/net/cnki/DemoApplication.java
+++ cnki-backend/cnki-web/src/main/java/net/cnki/DemoApplication.java
@@ -1,18 +1,11 @@
 package net.cnki;
 
-import org.activiti.engine.IdentityService;
-import org.activiti.engine.identity.Group;
-import org.activiti.engine.identity.User;
-import org.activiti.spring.boot.RestApiAutoConfiguration;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // 2.0的start中默认也有一个spring-boot-autoconfigure-2.0..RELEASE.jar，
 // 如果还引用了activiti的activiti-spring-boot-starter-rest-api.jar包，
